fd2773be192e3978bf7f4bc9117759df573e5e50
RyliusK <RyliusK@localhost>
2011-07-08 18:51:29 +0200

    finished tileset parsing

--- lib/parse_tmx.rb
+++ lib/parse_tmx.rb
@@ -14,6 +14,9 @@ module EH::Parse
       root = doc.root
       properties = {}
       tilesets = []
+      layers = []
+      objects = []
+      
       props = root.get_elements("properties")
       props.each { |prop|
         prop.each_element { |el|
@@ -21,24 +24,36 @@ module EH::Parse
         }
       }
       ap properties
+      
       ts = root.get_elements("tileset")
       ts.each { |set|
         img = set.get_elements("image").first
+        tile_props = {}
         tiles = set.get_elements("tile")
+        tiles.each { |tile|
+          hash = {}
+          tile.get_elements("properties/property").each { |p|
+            hash.store(p.attribute("name").to_s.to_sym, p.attribute("value").to_s)
+          }
+          tile_props.store(tile.attribute("id").to_s.to_i, hash)
+        }
         tilesets.push(
           EH::Tileset.new(
             set.attribute("firstgid"),
             set.attribute("name"),
             img.attribute("source").to_s,
-            {})
+            tile_props)
         )
       }
       ap tilesets
+      
       layers = root.get_elements("layer")
       layers.each { |layer|
         layer.each { |el|
         }
       }
+      ap layers
+      
       objectgrous = root.get_elements("objectgroup")
       objectgrous.each { |objectgroup|
         objectgroup.each { |el|
